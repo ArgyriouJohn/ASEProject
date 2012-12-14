751625ccc4e14e138f110117decb42986e5d72ce
raikky <iraikky@gmail.com>
2012-12-14 14:59:13 +0000

    Initial Commit : 1.Extend MainActivity class using MapActivity to show a map ,using the Google Maps API 2.Add a Location manager class in order to get gps data 3.Extend ItemizedOverlay class, so we can draw custom items/icon on the map. 4. Show an icon on user location 5. Create a custom 'bubble' that pops up when the user clicks on his icon.The bubble contaions location and date information.

---
+++ src/com/example/ase_map/CustomItemizedOverlay.java
@@ -0,0 +1,131 @@
+package com.example.ase_map;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.drawable.Drawable;
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.MapView;
+import com.google.android.maps.OverlayItem;
+
+public class CustomItemizedOverlay extends ItemizedOverlay
+{
+	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+	private Context mContext;
+	private GeoPoint p;
+	private Integer counter;
+	
+	public CustomItemizedOverlay(Drawable defaultMarker) 
+	{
+		super(boundCenterBottom(defaultMarker));
+	}
+	
+	public CustomItemizedOverlay(Drawable defaultMarker, Context context) 
+	{
+		super(boundCenterBottom(defaultMarker));
+		mContext = context;
+	}
+	
+	public void addOverlay(OverlayItem overlay) 
+	{
+	    mOverlays.add(overlay);
+	    populate();
+	}
+	
+	public void setOverlay(OverlayItem oldOverlay,OverlayItem newOverlay) 
+	{
+		int index = mOverlays.indexOf(oldOverlay);
+	    mOverlays.set(index,newOverlay);
+	    if(counter == null)
+		{
+			  counter=1;
+		}
+	    else if(counter%2 != 0)
+		{   // experimental!!! in case the bubble is open!
+			this.onTap(index);
+			this.onTap(index);
+		}
+	    populate();
+	}
+	
+	@Override
+	protected OverlayItem createItem(int i) 
+	{
+		return mOverlays.get(i);
+	}
+	
+	@Override
+	public int size() 
+	{
+		return mOverlays.size();
+	}
+	
+	@Override
+	protected boolean onTap(int index) 
+	{
+		  OverlayItem item = mOverlays.get(index);
+		  p = item.getPoint();
+		  // Show/Hide the bubble on Tap using a counter.
+		  if(counter == null)
+		  {
+			  counter=1;
+		  }
+		  else if(counter%2 != 0)
+		  {
+			  p = null; // if p=null , bubble gets removed. draw listens to that.
+			  counter++;
+		  }
+		  else 
+		  {
+			  counter++;
+		  }
+		  
+		  return true;
+	}
+	
+	 @Override
+     public boolean draw(Canvas canvas, MapView mapView, boolean shadow, long when) 
+     {
+		 super.draw(canvas, mapView, shadow); 
+		 // if p!=null create the bubble
+		 if(p!=null)
+		 {
+			 popupObject(canvas,mapView);
+		 }
+		 return true;
+     }
+	 
+	 public void popupObject(Canvas canvas, final MapView mapView)
+	 {
+		 // Date 
+		 Date dNow = new Date( );
+	     SimpleDateFormat hours = new SimpleDateFormat ("hh:mm:ss");
+	     SimpleDateFormat days = new SimpleDateFormat ("dd.MM.yy");
+	     
+         //---translate the GeoPoint to screen pixels---
+         Point screenPts = new Point();
+         mapView.getProjection().toPixels(p, screenPts);
+         
+         //text properties
+         Paint paint = new Paint();
+         paint.setTextAlign(Paint.Align.CENTER);
+         paint.setTextSize(20);
+         paint.setARGB(150, 0, 0, 0); // alpha, r, g, b (Black, semi see-through)
+
+         //---add the markers---
+         Bitmap bmp = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.sb);            
+         canvas.drawBitmap(bmp, screenPts.x -85, screenPts.y -220, null);
+         canvas.drawText(days.format(dNow), screenPts.x, screenPts.y -145, paint);
+         canvas.drawText(hours.format(dNow), screenPts.x, screenPts.y -165, paint);
+         canvas.drawText("Lat: "+String.valueOf(mapView.getMapCenter().getLatitudeE6()), screenPts.x, screenPts.y -105, paint);
+         canvas.drawText("Lon: "+String.valueOf(mapView.getMapCenter().getLongitudeE6()), screenPts.x, screenPts.y -125, paint); 
+	  }
+}
---
+++ src/com/example/ase_map/MainActivity.java
@@ -0,0 +1,156 @@
+package com.example.ase_map;
+
+import java.util.List;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
+
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.Menu;
+import android.widget.Toast;
+
+public class MainActivity extends MapActivity implements LocationListener 
+{
+	private MapView mapView;
+	private MapController mapController;
+	private LocationManager locationManager;
+	private String provider;
+	double latitude;
+    double longitude;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        // Get the location manager
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        boolean enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        
+        // Check if enabled and if not send user to the GSP settings
+        if (!enabled) 
+        {
+        	Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        	startActivity(intent);
+        } 
+        
+        // Define the criteria how to select the location in provider -> use default
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        Location location = locationManager.getLastKnownLocation(provider);
+        
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
+
+        // Initialize the location fields
+        if (location != null) 
+        {
+          System.out.println("Provider " + provider + " has been selected.");
+          //onLocationChanged(location);
+        } else 
+        {
+          //latituteField.setText("Location not available");
+          //longitudeField.setText("Location not available");
+        }
+        
+        // GPS Location
+        GeoPoint point = new GeoPoint((int)(latitude * 1E6), (int)(longitude *1E6));
+        
+        // Dispalay an itemizedoverlay on the map.
+        List<Overlay> mapOverlays = mapView.getOverlays();	
+        Drawable drawable = this.getResources().getDrawable(R.drawable.android96);
+        CustomItemizedOverlay itemizedoverlay = new CustomItemizedOverlay(drawable, this);
+        OverlayItem overlayitem = new OverlayItem(point," "," ");
+        itemizedoverlay.addOverlay(overlayitem);
+        mapOverlays.add(itemizedoverlay);
+        
+        // Initialize map fields.
+        mapController = mapView.getController();
+        mapController.animateTo(point);
+        mapController.setZoom(13);
+        mapView.invalidate();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) 
+    {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+    @Override
+    protected boolean isRouteDisplayed() 
+    {
+        return false;
+    }
+    
+    /* Request updates at startup */
+    @Override
+    protected void onResume() 
+    {
+      super.onResume();
+      boolean enabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+      if (!enabled) 
+      {
+      	Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+      	startActivity(intent);
+      } 
+      locationManager.requestLocationUpdates(provider, 400, 1, this);
+    }
+
+    /* Remove the locationlistener updates when Activity is paused */
+    @Override
+    protected void onPause() 
+    {
+      super.onPause();
+      locationManager.removeUpdates(this);
+    }
+
+    public void onLocationChanged(Location location) 
+    {   	
+    	latitude = location.getLatitude();
+    	longitude = location.getLongitude();
+    	GeoPoint point = new GeoPoint((int)(latitude * 1E6), (int)(longitude *1E6));
+    	
+    	// Update itemizedoverlay when location changes.
+    	List<Overlay> mapOverlays = mapView.getOverlays();
+        CustomItemizedOverlay itemizedoverlay = (CustomItemizedOverlay) mapOverlays.get(0);
+        OverlayItem oldOverlayitem = itemizedoverlay.getItem(0);
+        OverlayItem newOverlayitem = new OverlayItem(point," "," ");
+        itemizedoverlay.setOverlay(oldOverlayitem,newOverlayitem);
+        mapOverlays.set(0,itemizedoverlay);
+    	
+    	mapController.animateTo(point);
+    }
+
+    public void onStatusChanged(String provider, int status, Bundle extras) 
+    {
+    	// TODO Auto-generated method stub
+    }
+
+    public void onProviderEnabled(String provider) 
+    {
+    	Toast.makeText(this, "Enabled new provider " + provider,Toast.LENGTH_SHORT).show();
+    }
+
+    public void onProviderDisabled(String provider) 
+    {
+    	Toast.makeText(this, "Disabled provider " + provider,Toast.LENGTH_SHORT).show();
+    }
+
+}
